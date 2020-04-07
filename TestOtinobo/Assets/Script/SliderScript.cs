a9bb0c19e85c7db46b1538e6dccb924a6e03ba99
佐藤 <satourui7802@gmail.com>
2020-04-07 13:38:40 +0900

    プロジェクト移植完了

---
+++ TestOtinobo/Assets/Script/SliderScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    Slider slider;//スライダー
+
+    public GameObject player;//プレイヤーの蒸気の値を取得したい為
+    public PlayerScript script;//同上
+    float steampoint;//蒸気の値
+    // Start is called before the first frame update
+    void Start()
+    { 
+        slider = GameObject.Find("SteamSlider").GetComponent<Slider>();//スライダーを探す
+        Debug.Log(steampoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(steampoint);
+        steampoint = script.SteamPoint;
+        slider.value = steampoint;//値によって変動させる
+    }
+}
---
+++ TestOtinobo/Assets/Scriptes/CameraScritpt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScritpt : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    //このスクリプトは ごり押し適当仕様 なので後々作り直します
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(0, Player.transform.position.y + 4, -10);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
